8fde39688eed99e02817470e14cc2d7b517c56db
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-10-08 09:39:17 -0300

    aSDF

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,41 @@
-}”
+
+const boton = document.getElementById('btn');
+boton.addEventListener('click', () => { /*Es igual a function ()*/
+    let comments = document.getElementById('comment').value;
+    document.getElementById('comment').value = '';
+    const cont = document.getElementById('cont');
+    const newComments = document.createElement('div');
+    
+    const chck = document.createElement('input');
+    chck.type = 'checkbox';
+    
+    const heart = document.createElement('i');
+    heart.classList.add('fa', 'fa-heart', 'heart');
+    
+    const trash = document.createElement('i');
+    trash.classList.add('fa', 'fa-trash', 'trash');
+    
+    const contenedorElemento = document.createElement('p');
+    let textNewComment = document.createTextNode(comments);
+    contenedorElemento.appendChild(textNewComment);
+    newComments.appendChild(chck);
+    newComments.appendChild(heart);
+    newComments.appendChild(trash);
+    newComments.appendChild(contenedorElemento);
+    cont.appendChild(newComments);
+
+    heart.addEventListener('click', ()=> {
+        heart.classList.toggle('red');
+    })
+    trash.addEventListener('click', ()=> {
+        cont.removeChild(newComments);
+    })
+    chck.addEventListener('click', ()=> {
+        contenedorElemento.classList.toggle('strike-out');//linea horizontal sobre el texto
+    })
+}) 
+
+/*}”
 
   //validar que el textarea tenga un comentario
   if(comments.length === 0 || comments === null){
@@ -30,3 +67,6 @@
 
   cont.appendChild(newComments);
 })
+*/
+
+
